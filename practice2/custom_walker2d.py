5802de58649e8f335ce3339bf9e7ee99643166fb
gichul.hong <mdipok@gmail.com>
2026-08-12 00:11:59 +0900

    Doing bump2 with claude

--- practice2/custom_walker2d.py
+++ practice2/custom_walker2d.py
@@ -2,134 +2,162 @@ import numpy as np
 import gymnasium as gym
 import os
 
-# The observation space is a `Box(-Inf, Inf, (17,), float64)` where the elements are as follows:
-# | Num | Observation                                        | Min  | Max | Name (in corresponding XML file) | Joint | Type (Unit)              |
-# | --- | -------------------------------------------------- | ---- | --- | -------------------------------- | ----- | ------------------------ |
-# | 0   | x-coordinate of the torso                          | -Inf | Inf | rootz                            | slide | position (m)             |
-# | 1   | z-coordinate of the torso (height of Walker2d)     | -Inf | Inf | rootz                            | slide | position (m)             |
-# | 2   | angle of the torso                                 | -Inf | Inf | rooty                            | hinge | angle (rad)              |
-# | 3   | angle of the thigh joint                           | -Inf | Inf | thigh_joint                      | hinge | angle (rad)              |
-# | 4   | angle of the leg joint                             | -Inf | Inf | leg_joint                        | hinge | angle (rad)              |
-# | 5   | angle of the foot joint                            | -Inf | Inf | foot_joint                       | hinge | angle (rad)              |
-# | 6   | angle of the left thigh joint                      | -Inf | Inf | thigh_left_joint                 | hinge | angle (rad)              |
-# | 7   | angle of the left leg joint                        | -Inf | Inf | leg_left_joint                   | hinge | angle (rad)              |
-# | 8   | angle of the left foot joint                       | -Inf | Inf | foot_left_joint                  | hinge | angle (rad)              |
-# | 9   | velocity of the x-coordinate of the torso          | -Inf | Inf | rootx                            | slide | velocity (m/s)           |
-# | 10  | velocity of the z-coordinate (height) of the torso | -Inf | Inf | rootz                            | slide | velocity (m/s)           |
-# | 11  | angular velocity of the angle of the torso         | -Inf | Inf | rooty                            | hinge | angular velocity (rad/s) |
-# | 12  | angular velocity of the thigh hinge                | -Inf | Inf | thigh_joint                      | hinge | angular velocity (rad/s) |
-# | 13  | angular velocity of the leg hinge                  | -Inf | Inf | leg_joint                        | hinge | angular velocity (rad/s) |
-# | 14  | angular velocity of the foot hinge                 | -Inf | Inf | foot_joint                       | hinge | angular velocity (rad/s) |
-# | 15  | angular velocity of the thigh hinge                | -Inf | Inf | thigh_left_joint                 | hinge | angular velocity (rad/s) |
-# | 16  | angular velocity of the leg hinge                  | -Inf | Inf | leg_left_joint                   | hinge | angular velocity (rad/s) |
-# | 17  | angular velocity of the foot hinge                 | -Inf | Inf | foot_left_joint                  | hinge | angular velocity (rad/s) |
+BUMP_CONFIGS = {
+    "bump_practice": [
+        dict(name="bump1", x=6.0, width=0.3, height=0.2),
+        dict(name="bump2", x=10.0, width=0.6, height=0.45),
+    ],
+    "bump_challenge": [
+        dict(name="bump1", x=6.0, width=0.3, height=0.3),
+        dict(name="bump2", x=10.0, width=0.7, height=0.6),
+        dict(name="bump3", x=15.0, width=0.5, height=0.5),
+    ],
+}
 
-# The action space is a `Box(-1, 1, (6,), float32)`. An action represents the torques applied at the hinge joints.
-# | Num | Action                                 | Control Min | Control Max | Name (in corresponding XML file) | Joint | Type (Unit)  |
-# |-----|----------------------------------------|-------------|-------------|----------------------------------|-------|--------------|
-# | 0   | Torque applied on the thigh rotor      | -1          | 1           | thigh_joint                      | hinge | torque (N m) |
-# | 1   | Torque applied on the leg rotor        | -1          | 1           | leg_joint                        | hinge | torque (N m) |
-# | 2   | Torque applied on the foot rotor       | -1          | 1           | foot_joint                       | hinge | torque (N m) |
-# | 3   | Torque applied on the left thigh rotor | -1          | 1           | thigh_left_joint                 | hinge | torque (N m) |
-# | 4   | Torque applied on the left leg rotor   | -1          | 1           | leg_left_joint                   | hinge | torque (N m) |
-# | 5   | Torque applied on the left foot rotor  | -1          | 1           | foot_left_joint                  | hinge | torque (N m) |
 
 class CustomEnvWrapper(gym.Wrapper):
     def __init__(self, render_mode="human", bump_practice=False, bump_challenge=False):
         self.bump_practice = bump_practice
         self.bump_challenge = bump_challenge
+        self.is_bump_env = bump_practice or bump_challenge
+        self.bumps = []
+
         if bump_challenge:
+            self.bumps = BUMP_CONFIGS["bump_challenge"]
             env = gym.make(
                 "Walker2d-v5",
                 xml_file=os.getcwd() + "/asset/custom_walker2d_bumps.xml",
                 render_mode=render_mode,
                 exclude_current_positions_from_observation=False,
-                frame_skip = 10,
-                healthy_z_range=(0.5, 10.0))
+                frame_skip=10,
+                healthy_z_range=(0.9, 10.0))  # 무릎 보행(torso z~0.75-0.85) 차단
         elif bump_practice:
+            self.bumps = BUMP_CONFIGS["bump_practice"]
             env = gym.make(
                 "Walker2d-v5",
                 xml_file=os.getcwd() + "/asset/custom_walker2d_bumps_practice.xml",
                 render_mode=render_mode,
                 exclude_current_positions_from_observation=False,
-                frame_skip = 10,
-                healthy_z_range=(0.5, 10.0))
+                frame_skip=10,
+                healthy_z_range=(0.9, 10.0))  # 무릎 보행(torso z~0.75-0.85) 차단
         else:
             env = gym.make(
                 "Walker2d-v5",
                 render_mode=render_mode,
                 exclude_current_positions_from_observation=False,
-                frame_skip = 10)
-        
+                frame_skip=10)
+
         super().__init__(env)
-        
         self.prev_x = 0.0
-        self.passed_bump1 = False
-        self.passed_bump2 = False
-        
-        ## change observation space according to the new observation
+        self.passed_bumps = []
+
         obs, _ = self.reset()
-        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(len(obs),), dtype=np.float64)
-        
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf, high=np.inf, shape=(len(obs),), dtype=np.float64)
+
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
         self.prev_x = obs[0]
-        self.passed_bump1 = False
-        self.passed_bump2 = False
-        custom_obs = self.custom_observation(obs)
-        return custom_obs, info
+        self.passed_bumps = [False] * len(self.bumps)
+        self.face_bonus = [False] * len(self.bumps)
+        self.max_x = obs[0]
+        self.stall_steps = 0
+        return self.custom_observation(obs), info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
         custom_obs = self.custom_observation(obs)
-        custom_reward = self.custom_reward(obs, reward)
+        custom_reward = self.custom_reward(obs, action, reward)
         self.prev_x = obs[0]
-        custom_terminated = self.custom_terminated(terminated, obs)
-        custom_truncated = self.custom_truncated(truncated)
-        return custom_obs, custom_reward, custom_terminated, custom_truncated, info
+        terminated = self.custom_terminated(terminated, obs)
+        truncated = self.custom_truncated(truncated)
+        return custom_obs, custom_reward, terminated, truncated, info
 
     def custom_terminated(self, terminated, obs):
+        # 정지(standing) 국소최적 차단: 일정 시간 전진이 없으면 에피소드 종료.
+        # terminated로 끊어야 가치 부트스트랩이 없어 "서서 버티기"의 기대가치가 낮아짐.
+        if obs[0] > self.max_x + 0.05:
+            self.max_x = obs[0]
+            self.stall_steps = 0
+        else:
+            self.stall_steps += 1
+        if self.stall_steps >= 300:  # dt=0.02s -> 6초간 진전 없으면 종료 (범프 등반 시도 시간 확보)
+            return True
         return terminated
-    
+
     def custom_truncated(self, truncated):
         return truncated
 
     def custom_observation(self, obs):
-        if self.bump_practice or self.bump_challenge:
-            bump1_x = 6.0
-            bump2_x = 10.0
-            dist_to_bump1 = max(0.0, bump1_x - obs[0])
-            dist_to_bump2 = max(0.0, bump2_x - obs[0])
-            passed_bump1 = 1.0 if obs[0] > bump1_x else 0.0
-            passed_bump2 = 1.0 if obs[0] > bump2_x else 0.0
-            extra = np.array([dist_to_bump1 / 6.0, dist_to_bump2 / 10.0, passed_bump1, passed_bump2])
-            return np.concatenate([obs, extra])
-        return obs
+        torso_x = obs[0]
+        torso_z = obs[1]
+        extra = []
+        for b in self.bumps:
+            extra.append((b["x"] - torso_x) / 20.0)
+        extra.append(torso_z - 1.25)
+        return np.concatenate([obs, np.array(extra, dtype=np.float64)])
 
-    def custom_reward(self, obs, original_reward):
-        reward = original_reward
-        torso_angle_pen = -0.005 * abs(obs[2])
-        torso_vel_pen = -0.002 * abs(obs[11])
-        sym_pen = -0.003 * (abs(obs[3] - obs[6]) + abs(obs[4] - obs[7]) + abs(obs[5] - obs[8]))
-        reward += torso_angle_pen + torso_vel_pen + sym_pen
-        if self.bump_practice:
-            bump1_x = 6.0
-            bump2_x = 10.0
-            if not self.passed_bump1 and obs[0] > bump1_x:
-                reward += 50.0
-                self.passed_bump1 = True
-            if not self.passed_bump2 and obs[0] > bump2_x:
-                reward += 50.0
-                self.passed_bump2 = True
+    def custom_reward(self, obs, action, original_reward):
+        # 부호 있는 전진 보상: 후진은 그대로 벌점이 되므로
+        # 앞뒤 진동(제자리 호핑)으로 보상을 착취할 수 없음
+        dx = obs[0] - self.prev_x  # frame_skip=10 기준 1 step 이동 거리
+        torso_z = obs[1]
+        z_vel = obs[10]
+        torso_angle = obs[2]
+        torso_ang_vel = obs[11]
+        thigh_vel_r = obs[12]
+        thigh_vel_l = obs[15]
+
+        reward = 1.0  # healthy
+
+        # 전진 보상 (후진 시 페널티). dt=0.02s이므로 1 m/s 보행 시 dx=0.02
+        # -> 60*dx = 1.2/step으로 healthy(1.0)보다 크도록 스케일 조정.
+        # 높이 게이트: 몸을 낮추고(무릎 보행) 전진하면 전진 보상을 못 받음.
+        # torso_z >= 1.1이면 1.0, 0.9 이하면 0 (0.9 미만은 어차피 종료됨)
+        height_factor = np.clip((torso_z - 0.9) / 0.2, 0.0, 1.0)
+        reward += 60.0 * dx * height_factor
+
+        # 낮은 자세 페널티: 서있는 높이(~1.25)보다 낮게 웅크리면 벌점
+        reward -= 1.0 * max(0.0, 1.1 - torso_z)
+
+        # 자세 안정화 페널티 (정상 보행 시 스텝 보상이 양수를 유지하도록 약하게)
+        # z_vel 페널티는 "아직 안 넘은 범프 근처"에서는 끔: 범프를 넘으려면
+        # 큰 상승 속도가 필요한데 이걸 벌점 주면 발목 통통 점프로 수렴함
+        near_unpassed_bump = any(
+            not self.passed_bumps[i] and b["x"] - 2.5 <= obs[0] <= b["x"] + 1.0
+            for i, b in enumerate(self.bumps))
+        if not near_unpassed_bump:
+            reward -= 0.3 * (z_vel ** 2)
+        reward -= 0.3 * (torso_angle ** 2)
+        reward -= 0.05 * (torso_ang_vel ** 2)
+
+        # 교대 보행 보상: 양쪽 허벅지가 "반대 방향으로 움직일 때"만 보상.
+        # 정적 자세나 두 발 동시 호핑으로는 충족 불가.
+        swing_product = -thigh_vel_r * thigh_vel_l  # 반대 방향 스윙이면 양수
+        alternation = np.tanh(max(0.0, swing_product))
+        reward += 0.3 * alternation
+
+        if self.is_bump_env:
+            for i, b in enumerate(self.bumps):
+                # 범프 전면 도달 마일스톤(1회성 +15): "거의 넘음"과 "접근 못함"
+                # 사이에 보상 기울기를 만들어 등반 시도를 유도.
+                # x 기준 단조 마일스톤이라 진동으로 착취 불가.
+                if not self.face_bonus[i] and obs[0] > b["x"] - 0.4:
+                    reward += 15.0
+                    self.face_bonus[i] = True
+                if not self.passed_bumps[i] and obs[0] > b["x"]:
+                    reward += 50.0
+                    self.passed_bumps[i] = True
+
         return reward
 
-## Test Rendering
+
 if __name__ == "__main__":
     env = CustomEnvWrapper()
-    obs = env.reset()
+    obs, _ = env.reset()
     for _ in range(1000):
         action = env.action_space.sample()
         obs, reward, terminated, truncated, info = env.step(action)
         env.render()
-        if terminated:
-            obs = env.reset()
+        if terminated or truncated:
+            obs, _ = env.reset()
--- practice2/learning.py
+++ practice2/learning.py
@@ -16,15 +16,31 @@ policy_kwargs = dict(
 )
 
 import argparse
+import os
 parser = argparse.ArgumentParser()
 parser.add_argument("--bump_practice", action="store_true")
 parser.add_argument("--bump_challenge", action="store_true")
+parser.add_argument("--resume", type=str, default=None, help="Checkpoint .zip to resume training from")
 args = parser.parse_args()
 
 if __name__ == "__main__":
     env = SubprocVecEnv([make_env(bump_practice=args.bump_practice, bump_challenge=args.bump_challenge) for _ in range(N_ENVS)])
     env = VecMonitor(env)
-    env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=10.0)
+
+    vecnorm_path = None
+    if args.resume:
+        ckpt = args.resume.replace(".zip", "")
+        cand = ckpt.replace("walker_model_", "walker_model_vecnormalize_") + ".pkl"
+        if os.path.exists(cand):
+            vecnorm_path = cand
+
+    if vecnorm_path:
+        print(f"Loading VecNormalize stats from {vecnorm_path}")
+        env = VecNormalize.load(vecnorm_path, env)
+        env.training = True
+        env.norm_reward = False
+    else:
+        env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=10.0)
 
     if args.bump_practice:
         folder_name = "bump_practice"
@@ -42,9 +58,14 @@ if __name__ == "__main__":
         save_vecnormalize=True,
     )
 
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/", policy_kwargs=policy_kwargs, device="cpu",
-        learning_rate=3e-4, n_steps=2048, batch_size=64, gamma=0.995, ent_coef=0.0)
-
-    model.learn(total_timesteps=10000000000, callback=checkpoint_callback)
+    if args.resume:
+        print(f"Resuming from {args.resume}")
+        model = PPO.load(args.resume, env=env, tensorboard_log="./logs/", device="cpu")
+        model.ent_coef = 0.01  # 범프 등반 동작 탐색을 위해 상향
+        model.learn(total_timesteps=10000000000, callback=checkpoint_callback, reset_num_timesteps=False)
+    else:
+        model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/", policy_kwargs=policy_kwargs, device="cpu",
+            learning_rate=3e-4, n_steps=2048, batch_size=256, gamma=0.995, ent_coef=0.005, target_kl=0.03)
+        model.learn(total_timesteps=10000000000, callback=checkpoint_callback)
     model.save("ppo_custom_walker2d_parallel")
     env.save(f"{save_path}vecnormalize_final.pkl")
